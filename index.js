81f6097c700e79e56d04b7f179572a5135c96bdd
Michael Adegbite <adegbitemichael01@gmail.com>
2025-04-08 13:35:29 +0100

    basics of CRUD

--- index.js
+++ index.js
@@ -7,47 +7,101 @@ dotEnv.config()
 app.use(express.json())
 const studentInfo = require("./schema")
 
-mongoose.connect(`${process.env.MONGOOSE_URL }`)
-.then(() => console.log('mongoose connected...'))
+mongoose.connect(`${process.env.MONGOOSE_URL}`)
+    .then(() => console.log('mongoose connected...'))
 
 // const studentInfo = require("./schema")
 
-app.use(express.json())
+// app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('sever connected...' + PORT)
 })
 
-app.post("/students", async (req, res) => {
-    const {firstName,lastName,email, age,phoneNumber} = req.body
+//CRUD OPERATIONS
+//create
+app.post("/register", async (req, res) => {
+    const { firstName, lastName, email, passWord, cf_password, age, phoneNumber } = req.body
 
-    if(!email){
-       return res.status(400).json({message: "email require"})
+    if (!email) {
+        return res.status(400).json({ message: "email require" })
     }
-    const alreadyExisted = await studentInfo.findOne({email})
-    if(alreadyExisted){
-        return res.status(400).json({message:'user already existed'})
+    if (age < 18) {
+        return res.status(400).json({ message: "can't register" })
     }
-    const newStudent  = new studentInfo({firstName,lastName,email, age,phoneNumber})
+    if (cf_password !== passWord) {
+        return res.status(400).json({ message: "confirm password " })
+    }
 
+    const alreadyRegister = await studentInfo.findOne({ email })
+    if (alreadyRegister) {
+        return res.status(400).json({ message: "user already existed" })
+    }
+
+    const newStudent = new studentInfo({ firstName, lastName, email, passWord, age, phoneNumber })
     await newStudent.save()
 
-     res.status(200).json({
-        message:'successful',
-        user: newStudent
+
+    res.status(200).json({ message: 'Registration successful', newStudent })
+})
+
+
+//read
+app.get("/students", async (req, res) => {
+    const allStudents = await studentInfo.find()
+
+    res.status(200).json({
+        message: 'Registration successful',
+        count: allStudents.length,
+        allStudents
     })
+})
+
+//put/patch
+
+app.put("/updateUser/:id", async (req, res) => {
+    const { id } = req.params
+    const { firstName, lastName } = req.body
+    const updateThisUser = await studentInfo.findByIdAndUpdate(id, { firstName, lastName }, { new: true })
+
+    const User = { firstName, lastName }
+
+    res.status(200).json({ message: 'Registration successful', updateThisUser })
+})
+
+//delete
+app.delete("/delete_studentId/:Id", async (req, res) => {
+    const { id } = req.params
+
+    const deleteUser = await studentInfo.findByIdAndDelete(id)
+
+    res.status(200).json({ message: 'Successful' })
 
 })
 
-app.get("/students", async (req,res)=>{
-    const findAll = await studentInfo.find()
+app.get("/fund_wallet",async (req,res) => {
+    const {email, amount} = req.body
 
-    return res.status(200).json({
-        message:"sucessful",
-        count:findAll.length,
-        user: findAll,
-    })
+    const user = await studentInfo.findOne({email})
+
+    user.walletBalance += Number(amount)
+    await user.save()
+    res.status(200).json({message:'Successful', user})
 })
 
+//get user details wit thier funds
+app.get("/getUser_funds/:id", async (req, res) => {
+    const { id } = req.params
+    // const { email, amount } = req.body
+
+    const user = await studentInfo.findById(id)
+
+
+    // user.walletBalance += Number(amount)
+
+    // await user.save()
+
+    res.status(200).json({ message: 'Successful', user })
+})
 
--- schema.js
+++ schema.js
@@ -2,11 +2,13 @@
 const mongoose = require('mongoose')
 
 const Student = new mongoose.Schema({
-    firstName: {type: String},
-    lastName: {type:String},
-    email: {type:String},
-    age:{type:Number},
-    phoneNumber: {type: Number}
+    firstName: {type: String, require},
+    lastName: {type:String, require},
+    email: {type:String, require},
+    passWord:{type:String, require},
+    age:{type:Number, require, require},
+    phoneNumber: {type: Number, require},
+    walletBalance:{type:Number, default: 0}
 })
 
 const studentInfo = new mongoose.model('student', Student)
